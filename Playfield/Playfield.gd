a84b62456326e6710b004c13041e47e688d68a36
Daniel Cannell <danielcannell@googlemail.com>
2019-10-05 14:06:06 +0100

    Add an enemy

--- Playfield/Playfield.gd
+++ Playfield/Playfield.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
-const Player = preload("res://Playfield/Player.tscn")
+
+const Player = preload("res://Playfield/Entities/Player.tscn")
+const Enemy = preload("res://Playfield/Entities/Enemy.tscn")
+
+
+var enemies = []
 
 
 func _init():
     var player = Player.instance()
     add_child(player)
+
+    var enemy = Enemy.instance()
+    add_child(enemy)
+    enemies.append(enemy)
