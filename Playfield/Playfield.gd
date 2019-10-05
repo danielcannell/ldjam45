3546f58c788a7d8e96aa57822d99087ae2882c27
Jamie Wood <jamie.wood70@ntlworld.com>
2019-10-05 14:32:50 +0100

    Add camera pan-to-room

--- Playfield/Playfield.gd
+++ Playfield/Playfield.gd
@@ -6,7 +6,9 @@ const Enemy = preload("res://Playfield/Entities/Enemy.tscn")
 
 
 var enemies = []
-
+var rooms = []
+onready var tilemap = find_node("TileMap")
+onready var roomcenter = find_node("RoomCenter")
 
 func _init():
     var player = Player.instance()
@@ -15,3 +17,23 @@ func _init():
     var enemy = Enemy.instance()
     add_child(enemy)
     enemies.append(enemy)
+
+    rooms.append(Rect2(0, 0, 9, 9))
+    rooms.append(Rect2(8, 0, 11, 14))
+    rooms.append(Rect2(0, 8, 11, 10))
+
+
+func _ready():
+    roomcenter.cell_size = tilemap.cell_size
+    roomcenter.jump_to_room(rooms[0])
+
+
+func _unhandled_input(event):
+    if event is InputEventKey:
+        if event.pressed:
+            if (event.scancode == KEY_1):
+                roomcenter.move_to_room(rooms[0])
+            if (event.scancode == KEY_2):
+                roomcenter.move_to_room(rooms[1])
+            if (event.scancode == KEY_3):
+                roomcenter.move_to_room(rooms[2])
--- Playfield/RoomCenter.gd
+++ Playfield/RoomCenter.gd
@@ -2,15 +2,61 @@ extends Node2D
 
 const SPEED = 4
 
-func _unhandled_input(event):
-    if event is InputEventKey:
-        if event.pressed:
-            if (event.scancode == KEY_A):
-                self.position += Vector2(-SPEED, 0)
-            if (event.scancode == KEY_D):
-                self.position += Vector2(SPEED, 0)
-            if (event.scancode == KEY_W):
-                self.position += Vector2(0, -SPEED)
-            if (event.scancode == KEY_S):
-                self.position += Vector2(0, SPEED)
-    print(event)
+onready var playfield = find_parent("Playfield")
+onready var camera = find_node("Camera2D")
+var targetzoom = 1.0
+var targetposition = Vector2(0, 0)
+
+var cell_size = Vector2(16, 16)
+
+func jump_to_room(room: Rect2):
+    move_to_room(room)
+    camera.zoom = Vector2(targetzoom, targetzoom)
+    position = targetposition
+
+
+func move_to_room(room: Rect2):
+    var left = room.position.x
+    var top = room.position.y
+    var width = room.size.x
+    var height = room.size.y
+
+    # TODO work out screen size, in cells
+    var screenheight = 600 / cell_size.y
+    var screenwidth = 1024 / cell_size.x
+
+    var screenscale = 1.0
+
+    # Scale so the largest dimension fits on screen
+    if (screenheight / screenwidth) < (height / width):
+        # screen is wider than map is tall: scale so height fits
+        screenscale = height / screenheight
+    else:
+        # screen is taller than map is wide: scale so width fits
+        screenscale = width / screenwidth
+
+    targetzoom = screenscale
+    targetposition = Vector2(left + width / 2, top + height / 2) * cell_size.x
+
+
+func _physics_process(delta):
+    var currentzoom = camera.zoom.x
+    if (abs(currentzoom - targetzoom) > 0.001):
+        var z = 1.0
+        if (((currentzoom <= targetzoom) and (currentzoom / targetzoom > 0.99)) or
+            ((currentzoom >= targetzoom) and (targetzoom / currentzoom > 0.99))):
+            z = targetzoom
+            print("jumpzoom")
+        else:
+            z = lerp(currentzoom, targetzoom, 0.1)
+        camera.zoom = Vector2(z, z)
+
+    var currentpos = position
+    if (currentpos != targetposition):
+        var pos = Vector2(0, 0)
+        if ((currentpos - targetposition).length() < 0.25):
+            pos = targetposition
+            print("jumppos")
+        else:
+            pos = lerp(currentpos, targetposition, 0.1)
+        position = pos
