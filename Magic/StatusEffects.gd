852c27831d1dabbbbdd200e0814379b590efe7b4
Aidan Woolley <awoolley10@gmail.com>
2019-10-05 16:15:01 +0100

    Make resistances dict values multipliers

--- Magic/StatusEffects.gd
+++ Magic/StatusEffects.gd
@@ -2,11 +2,11 @@ extends Node
 class_name StatusEffects
 
 var resistances := {
-	"fire": false,
-	"water": false,
-	"wind": false,
-	"rock": false, 
-	"physical": false 
+	"fire": 1.0,
+	"water": 1.0,
+	"wind": 1.0,
+	"rock": 1.0, 
+	"physical": 1.0 
 }
 
 var buffs := {
