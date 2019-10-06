20b11790b620b362ab967134ec8f464057b3851e
Daniel Cannell <danielcannell@googlemail.com>
2019-10-06 14:26:56 +0100

    Equip and disenchant signals

--- UI/UI.gd
+++ UI/UI.gd
@@ -5,29 +5,31 @@ const ComponentButton := preload("res://UI/ComponentButton.gd")
 const FocusButton := preload("res://UI/FocusButton.gd")
 
 
-var components := [
-    Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.FIRE),
-    Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.WATER),
-    Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.WIND),
-    Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.ROCK),
-]
-
-
-var foci := [
-    Focus.new(Globals.FocusType.HAT, Globals.Foci.HAT, Globals.Action.MULTIPLIER, null, 0.0),
-    Focus.new(Globals.FocusType.WEAPON, Globals.Foci.STICK, Globals.Action.MULTIPLIER, null, 0.0),
-    Focus.new(Globals.FocusType.WEAPON, Globals.Foci.STICK, Globals.Action.MULTIPLIER, components[0], 0.0),
-    Focus.new(Globals.FocusType.WEAPON, Globals.Foci.WAND, Globals.Action.MULTIPLIER, components[1], 0.0),
-]
+func on_inventory_changed(components, foci):
+    update_components_list(components)
+    update_focus_list(foci)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    update_components_list()
-    update_focus_list()
+    var components := [
+        Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.FIRE),
+        Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.WATER),
+        Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.WIND),
+        Component.new(Globals.ComponentType.ELEMENT, Globals.Elements.ROCK),
+    ]
+
+    var foci := [
+        Focus.new(Globals.FocusType.HAT, Globals.Foci.HAT, Globals.Action.MULTIPLIER, null, 0.0),
+        Focus.new(Globals.FocusType.WEAPON, Globals.Foci.STICK, Globals.Action.MULTIPLIER, null, 0.0),
+        Focus.new(Globals.FocusType.WEAPON, Globals.Foci.STICK, Globals.Action.MULTIPLIER, components[0], 0.0),
+        Focus.new(Globals.FocusType.WEAPON, Globals.Foci.WAND, Globals.Action.MULTIPLIER, components[1], 0.0),
+    ]
+
+    on_inventory_changed(components, foci)
 
 
-func update_components_list():
+func update_components_list(components):
     var component_list := $CanvasLayer/Panel/VBoxContainer/ComponentContainer/ComponentList
 
     for c in components:
@@ -35,7 +37,7 @@ func update_components_list():
         component_list.add_child(btn)
 
 
-func update_focus_list():
+func update_focus_list(foci):
     var focus_list := $CanvasLayer/Panel/VBoxContainer/FociContainer/FociList
 
     for f in foci:
