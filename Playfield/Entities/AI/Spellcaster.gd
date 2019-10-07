4935062b2f1168fa9703cefb136d18051db2af9a
Jamie Wood <jamie.wood70@ntlworld.com>
2019-10-07 21:14:10 +0100

    Make spellcasters give up if the player leaves the room

--- Playfield/Entities/AI/Spellcaster.gd
+++ Playfield/Entities/AI/Spellcaster.gd
@@ -7,6 +7,7 @@ class_name AISpellcaster
 
 
 var tracking_player: Node2D = null
+var current_room = null
 
 var see_range: float = 0
 var lose_sight_range: float = 0
@@ -21,20 +22,25 @@ func _init(manager, entity, see_range: int, lose_sight_range: int, attack_range:
 
 
 func think():
-    if not tracking_player:
+    if current_room == null:
+        current_room = manager.get_room(get_pos())
+
+    var player_room = manager.get_room(player_pos())
+
+    if not tracking_player and player_room == current_room:
         tracking_player = get_nearby_player(see_range)
 
     if tracking_player:
         var dist := get_pos().distance_to(tracking_player.position)
-        if dist < keep_away_range:
+        if dist > lose_sight_range or player_room != current_room:
+            tracking_player = null
+            return GoalHelpers.idle()
+        elif dist < keep_away_range:
             var diff := tracking_player.position - get_pos()
             var dir := diff.normalized()
             return GoalHelpers.go_to(get_pos() - (dir * 1000))
         elif dist < attack_range:
             return GoalHelpers.attack(tracking_player, true)
-        elif dist > lose_sight_range:
-            tracking_player = null
-            return GoalHelpers.idle()
         else:
             return GoalHelpers.go_to(tracking_player.position)
 
